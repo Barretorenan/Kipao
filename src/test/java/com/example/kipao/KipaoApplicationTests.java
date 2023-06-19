bf87bba72b9989632aebbfac66b55011ea81c032
Barreto <barretorenan1@gmail.com>
2023-06-19 01:02:04 -0300

    pre set up

---
+++ src/test/java/com/example/kipao/KipaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kipao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KipaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
