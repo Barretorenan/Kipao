079b57bcd50ebbb71ccadebf1b8c1e7abcfd4fe8
raphael.sampaio2 <raphael.sampaio@edu.unirio.br>
2023-07-10 17:57:02 -0300

    Crud Assinatura

--- src/main/java/com/example/kipao/repository/AssinaturaRepository.java
+++ src/main/java/com/example/kipao/repository/AssinaturaRepository.java
@@ -2,9 +2,11 @@ package com.example.kipao.repository;
 
 import com.example.kipao.model.Assinatura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AssinaturaRepository extends JpaRepository<Assinatura, Integer> {
     List<Assinatura> findAllByPedido_ClienteId(int id);
 }
