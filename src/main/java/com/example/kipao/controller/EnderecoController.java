943f4d89b2f5b6cd027d370a7a2b4e7d5bcc5dd1
raphael.sampaio2 <raphael.sampaio@edu.unirio.br>
2023-07-10 13:09:13 -0300

    endereco fix

--- src/main/java/com/example/kipao/controller/EnderecoController.java
+++ src/main/java/com/example/kipao/controller/EnderecoController.java
@@ -1,7 +1,9 @@
 package com.example.kipao.controller;
 
 import com.example.kipao.model.Endereco;
+import com.example.kipao.model.Usuario;
 import com.example.kipao.repository.EnderecoRepository;
+import com.example.kipao.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,9 +11,11 @@ import java.util.List;
 @RestController
 public class EnderecoController {
     private final EnderecoRepository enderecoRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public EnderecoController(EnderecoRepository enderecoRepository){
+    public EnderecoController(EnderecoRepository enderecoRepository, UsuarioRepository usuarioRepository){
         this.enderecoRepository = enderecoRepository;
+        this.usuarioRepository = usuarioRepository;
     }
     @GetMapping("/enderecos")
     public List<Endereco> recuperarEnderecos(){
@@ -23,8 +27,10 @@ public class EnderecoController {
         return enderecoRepository.findById(id).get();
     }
 
-    @PostMapping("/'criar-endereco'")
-    public Endereco criarEndereco(@RequestBody Endereco endereco){
+    @PostMapping("/criar-endereco/{id}")
+    public Endereco criarEndereco(@PathVariable int id, @RequestBody Endereco endereco){
+        Usuario usuarioRecuperado = usuarioRepository.findById(id).get();
+        endereco.setUsuario(usuarioRecuperado);
         return enderecoRepository.save(endereco);
     }
 
